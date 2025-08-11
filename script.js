d2b677bd9bc9aa1df9f2d4bfee37b2333339b923
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-11 20:24:20 -0300

    alterações feitas

--- script.js
+++ script.js
@@ -1,51 +1,25 @@
-class Sorteador {
-  constructor(qtdNumeros, numeroMinimo, numeroMaximo, botaoIniciar) {
-    this.qtdNumeros = document.getElementById("qtd-numeros");
-    this.numeroMinimo = document.getElementById("numero-minimo");
-    this.numeroMaximo = document.getElementById("numero-maximo");
-    this.botaoIniciar = document.getElementById("sort-button");
+const sortBtn = document.getElementById("sort-button");
+const quantidadeNumeros = document.getElementById("quantidade-numeros");
+const numeroMinimo = document.getElementById("numero-minimo");
+const numeroMaximo = document.getElementById("numero-maximo");
+
+
+const sortearNumeros = () => {
+  const qtdNumeros = quantidadeNumeros.value;
+  const min = numeroMinimo.value;
+  const max = numeroMaximo.value;
+
+  if (qtdNumeros > max - min + 1) {
+    alert("A quantidade de números sorteados não pode ser maior do que a diferença entre o número máximo e mínimo.");
+    return;
   }
 
-  inicializar() {
-    this.botaoIniciar.addEventListener("click", sortear);
-  }
-
-  sortear() {
-    let qtdNumeros = this.qtdNumeros.value;
-    let numeroMinimo = this.numeroMinimo.value;
-    let numeroMaximo = this.numeroMaximo.value;
-
-    let numerosSorteados = [];
-
-    for (let i = 0; i <= qtdNumeros; i++) {
-      let numeroSorteado =
-        Math.floor(Math.random() * (numeroMaximo - numeroMinimo + 1)) +
-        numeroMinimo;
+  const numerosSorteados = [];
+  for (let i = 0; i <= qtdNumeros; i++) {
+    const numeroSorteado = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (!numerosSorteados.includes(numeroSorteado)) {
       numerosSorteados.push(numeroSorteado);
     }
-
-    console.log(numerosSorteados);
-
-    if (!numeroMinimo || !numeroMaximo) {
-      alert("Por favor, insira valores válidos.");
-      return;
-    }
-
-    if (qtdNumeros > numeroMaximo - numeroMinimo + 1) {
-      alert(
-        "A quantidade de números sorteados não pode exceder a diferença entre o número máximo e o número mínimo."
-      );
-      return;
-    }
-
-    if (qtdNumeros <= 0) {
-      alert("Por favor, insira uma quantidade válida de números.");
-      return;
-    }
-
-    if (numeroMinimo > numeroMaximo) {
-      alert("O número mínimo não pode ser maior que o número máximo.");
-      return;
-    }
   }
+
 }
