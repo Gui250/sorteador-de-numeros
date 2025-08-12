3a8c65f4d79b3865e8aebded34686008f282712e
Guilherme Moreno <guilhermemoreno200@gmail.com>
2025-08-12 13:14:08 -0300

    estilos aplicados

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const numeroMaximo = document.getElementById("numero-maximo");
 const inputWrapper = document.getElementById("input-wrapper");
 const sortContainer = document.getElementById("sort-container");
 const btnNaoRepetir = document.querySelector(".toggle-input");
+const toggleContainer = document.querySelector(".toggle-container");
+const sortTextContainer = document.getElementById("sort-text-container");
+let contador = 0;
+
 const sortearNumeros = () => {
   const qtdNumeros = parseInt(quantidadeNumeros.value);
   const min = parseInt(numeroMinimo.value);
@@ -48,7 +52,28 @@ const sortearNumeros = () => {
     }
   }
 
+  // Incrementa o contador após gerar os números
+  contador++;
+
   // Remove o input wrapper e substitui pelo container dos números sorteados
+  const textResultContainer = document.createElement("div");
+  textResultContainer.classList.add("text-result-container");
+  const titleSort = document.createElement("h2");
+  titleSort.classList.add("title-sort");
+  const paragraphSort = document.createElement("p");
+  paragraphSort.classList.add("paragraph-sort");
+
+  titleSort.innerText = "Números Sorteados:";
+  paragraphSort.innerText = `${contador} º resultado`;
+
+  textResultContainer.appendChild(titleSort);
+  textResultContainer.appendChild(paragraphSort);
+
+  sortTextContainer.parentNode.replaceChild(
+    textResultContainer,
+    sortTextContainer
+  );
+
   const numeroSorteadoContainer = document.createElement("div");
   numeroSorteadoContainer.classList.add("numero-sorteado-container");
 
@@ -59,6 +84,8 @@ const sortearNumeros = () => {
     .join(", ");
 
   numeroSorteadoContainer.appendChild(numeroSorteadoText);
+
+  toggleContainer.style.display = "none";
 
   // Substitui o input-wrapper pelo numeroSorteadoContainer
   inputWrapper.parentNode.replaceChild(numeroSorteadoContainer, inputWrapper);
@@ -82,8 +109,13 @@ const sortearNumeros = () => {
     quantidadeNumeros.value = "";
     numeroMinimo.value = "";
     numeroMaximo.value = "";
+    toggleContainer.style.display = "block";
 
     tentarNovamenteBtn.parentNode.replaceChild(sortBtn, tentarNovamenteBtn);
+    textResultContainer.parentNode.replaceChild(
+      sortTextContainer,
+      textResultContainer
+    );
   });
 };
 
